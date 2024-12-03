6c6e23bd95d12c2fe980c325e3d6eacaf3dd179c
Graham Strickland <grahamhstrickland@gmail.com>
2024-12-03 11:31:07 +0200

    Add Pico CSS to example app

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from fasthtml.common import (
     FastHTML,
     H1,
+    Main,
+    picolink,
     serve,
+    Style,
+    Title,
 )
 
-app = FastHTML()
+
+css = Style(":root {--pico-font-size:90%,--pico-font-family: Pacifico, cursive;}")
+app = FastHTML(hdrs=(picolink, css))
 
 
 @app.get("/")
 def home():
-    return H1("Hello, World")
+    return (Title("Hello World"), Main(H1("Hello, World"), cls="container"))
 
 
 @app.route("/", methods=["post", "put"])
