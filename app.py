717d2cc23f626f3d4c9802dff61af615884d7875
Graham Strickland <grahamhstrickland@gmail.com>
2024-12-03 11:14:31 +0200

    Add HTML elements to example app

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from fasthtml.common import FastHTML, serve
+from fasthtml.common import (
+    Div,
+    FastHTML,
+    H1,
+    P,
+    serve,
+    Title,
+)
 
 app = FastHTML()
 
 
 @app.get("/")
 def home():
-    return "<h1>Hello, World</h1>"
+    return (
+        Title("Page Demo"),
+        Div(H1("Hello, World")),
+        P("Some text"),
+        P("Some more text"),
+    )
 
 
 serve()
